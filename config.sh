6044460cc2428779526b233a34ffca0e2837ea63
Aryan111 <45229110+Aryan111@users.noreply.github.com>
2023-09-30 22:38:13 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash env
 
 # about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export NAME="pbrp" # supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb.git" # the link of manifest
+export BRANCH="android-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="RMX2170L1" # codename used in device tree
+export DT_LINK="https://github.com/Aryan111/android_device_realme_RMX2170L1.git" # device tree link
+export DT_BRANCH="android-12.1" # device tree branch
+export VENDOR="realme" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
